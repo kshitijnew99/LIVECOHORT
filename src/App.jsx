63c1e076604092edb5683cfcd3d6869fffb46dd2
kshitijraoranjannew99@gmail.com <kshitijraoranjannew99@gmail.com>
2025-06-08 12:05:09 +0530

    react update

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,18 @@ import { useState } from "react"
 const App = () => {
 
 
-  const [name,setname] = useState("kshitij");
+  const [Username,setname] = useState("kshitij");
 
 
   const changeName = () => {
     setname("piyush");
   }
-  console.log(name);
+  console.log(Username);
   
   
   return (<>
             <h1>UserName</h1>
-            <h3>{name}</h3>
+            <h3>{Username}</h3>
             
             
 
